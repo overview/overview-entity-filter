a89dfe9b677e2074af14b497e8502fc5e1bd7ca4
Adam Hooper <adam@adamhooper.com>
2020-03-17 13:44:47 -0400

    server.js: Clean after decaffeinate

--- server/server.js
+++ server/server.js
@@ -1,14 +1,8 @@
-/*
- * decaffeinate suggestions:
- * DS102: Remove unnecessary code created because of implicit returns
- * Full docs: https://github.com/decaffeinate/decaffeinate/blob/master/docs/suggestions.md
- */
 const app = require('./app');
 
 const port = process.env.PORT || 3000;
 
-var server = app.listen(port, function() {
+const server = app.listen(port, function() {
   const addr = server.address();
-
-  return console.log("Listening at http://%s:%d", addr.address, addr.port);
+  console.log("Listening at http://%s:%d", addr.address, addr.port);
 });
